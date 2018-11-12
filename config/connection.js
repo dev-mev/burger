f4536e79dbaf249afaa8f691ee9bb37cf14a4a87
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-11-12 12:51:04 -0800

    edit connection.js to include JAWSDB info

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ const mysql = require("mysql");
 // require .env file with mysql connection info
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+  });
+}
 
 // Make connection.
 connection.connect(function (err) {
